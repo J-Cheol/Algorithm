dc78b444eb89a94a26fcc9438e8ac71232961563
전화철 <117714970+J-Cheol@users.noreply.github.com>
2024-01-12 22:20:45 +0900

    [level Lv.0] Title: 문자열 곱하기, Time: 1.57 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -0,0 +1,13 @@
+class Solution 
+{
+    public String solution(String my_string, int k) 
+    {
+        String answer = "";
+        while(k > 0)
+        {
+            answer += my_string;
+            k--;
+        }
+        return answer;
+    }
+}
