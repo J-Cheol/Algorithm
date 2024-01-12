eef8f7d11b829c50eb4ee965126f9e38c7bd2064
전화철 <117714970+J-Cheol@users.noreply.github.com>
2024-01-13 00:55:19 +0900

    [level Lv.0] Title: 대문자로 바꾸기, Time: 0.02 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
+++ 프로그래머스/Lv.0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -1,6 +1,18 @@
-class Solution {
-    public String solution(String myString) {
-        String answer = myString.toUpperCase();
+class Solution 
+{
+    public String solution(String myString) 
+    {
+        // String answer = myString.toUpperCase();
+        String answer = "";
+        
+        char[] alphabets = myString.toCharArray();
+        for (char alphabet : alphabets)
+        {
+            if(alphabet >= 'a' && alphabet <= 'z')
+                answer += (char)(alphabet - 32);
+            else
+                answer += alphabet;
+        }
         return answer;
     }
 }
