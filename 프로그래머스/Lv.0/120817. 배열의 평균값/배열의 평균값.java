2bb36ec26572d0134db137c7378a72eb3010681a
전화철 <117714970+J-Cheol@users.noreply.github.com>
2024-01-12 16:32:08 +0900

    [level Lv.0] Title: 배열의 평균값, Time: 0.05 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120817. 배열의 평균값/배열의 평균값.java
@@ -0,0 +1,13 @@
+class Solution 
+{
+    public double solution(int[] numbers) 
+    {
+        double answer = 0;
+        for (int num : numbers)
+        {
+            answer += num;
+        }
+        answer /= numbers.length;
+        return answer;
+    }
+}
