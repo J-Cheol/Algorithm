6854e3b95c1a53733f0b111d8fb880e6c8dda8c3
전화철 <117714970+J-Cheol@users.noreply.github.com>
2024-01-19 22:55:24 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 3.08 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -1,29 +1,10 @@
-import java.util.List;
-import java.util.ArrayList;
-class Solution 
-{
-    public int[] solution(int n) 
-    {
-        List<Integer> collatz = new ArrayList<>();
-        collatz.add(n);
-        while(n > 1)
-        {
-            if(n % 2 == 0)
-            {
-                n = n / 2;
-                collatz.add(n);
-            }
-            else if(n % 2 == 1)
-            {
-                n = (3 * n) + 1;
-                collatz.add(n);
-            }
-        }
-        int[] answer = new int[collatz.size()];
-        for(int i = 0; i < collatz.size(); i++)
-        {
-            answer[i] = collatz.get(i);
-        }
-        return answer;
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[] solution(int n) {
+        return IntStream.concat(
+                        IntStream.iterate(n, i -> i > 1, i -> i % 2 == 0 ? i / 2 : i * 3 + 1),
+                        IntStream.of(1))
+                .toArray();
     }
 }
