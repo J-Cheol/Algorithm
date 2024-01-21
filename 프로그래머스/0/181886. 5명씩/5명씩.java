3f1e26cd65262b4d0b2f4d78e64eab22aa05f07d
전화철 <117714970+J-Cheol@users.noreply.github.com>
2024-01-21 21:00:54 +0900

    [level 0] Title: 5명씩, Time: 0.02 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.java
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -10,9 +10,8 @@ class Solution
         else if(names.length % 5 != 0)
             len = names.length / 5 + 1;
         String[] answer = new String[len];
-        answer[0] = names[0];
-        int j = 1;
-        for(int i = 1; i < names.length; i++)
+        int j = 0;
+        for(int i = 0; i < names.length; i++)
         {
             if(i % 5 == 0)
                 answer[j++] = names[i];
