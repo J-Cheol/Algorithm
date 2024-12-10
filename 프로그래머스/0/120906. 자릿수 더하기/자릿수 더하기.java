6bd641fd3559d8ad5a774d9d0c2badcf3175982b
전화철 <117714970+J-Cheol@users.noreply.github.com>
2024-12-10 23:39:58 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,15 @@
+class Solution 
+{
+    public int solution(int n) {
+        int answer = plus(n, 0);
+        
+        return answer;
+    }
+    
+    public static int plus(int n, int sum){
+        sum += n % 10;
+        if(n == 0)
+            return sum;
+        return plus(n / 10, sum);
+    }
+}
