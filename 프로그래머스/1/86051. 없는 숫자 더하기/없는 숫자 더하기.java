33fc8fdf3796f2e7793bccec4038461df4ebec8b
전화철 <117714970+J-Cheol@users.noreply.github.com>
2025-02-27 21:36:59 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 87.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 45;
+        int sum = 0;
+        for(int num : numbers) {
+            sum += num;
+        }
+        
+        answer -= sum;
+        return answer;
+    }
+}
