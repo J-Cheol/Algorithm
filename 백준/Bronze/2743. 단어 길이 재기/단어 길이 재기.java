5950e6f64274a2c3a7f9cabd223b16e9f9b9931d
전화철 <117714970+J-Cheol@users.noreply.github.com>
2025-05-28 00:42:10 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 104 ms, Memory: 12872 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int length = sc.nextLine().length();
+		System.out.println(length);
+
+		sc.close();
+	}
+}
