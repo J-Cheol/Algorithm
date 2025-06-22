40829572d9b12ae88c53a1943316e916322b03d0
전화철 <117714970+J-Cheol@users.noreply.github.com>
2025-06-22 21:44:52 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 92 ms, Memory: 12884 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		int GCD = gcd(a, b);
+		int LCM = (a * b) / GCD;
+
+		System.out.println(GCD);
+		System.out.println(LCM);
+	}
+
+	private static int gcd(int a, int b) {
+		while (b != 0) {
+			int r = a % b;
+			a = b;
+			b = r;
+		}
+		return a;
+	}
+}
