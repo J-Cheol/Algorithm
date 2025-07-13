c724ae6e5a8178517a794c43678df4b6d9ad517c
전화철 <117714970+J-Cheol@users.noreply.github.com>
2025-07-13 23:56:18 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 104 ms, Memory: 12836 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String str = sc.nextLine();
+		int[] alphabet = new int[26];
+
+		for(int i = 0; i < str.length(); i++) {
+			alphabet[str.charAt(i) - 'a']++;
+		}
+
+		for(int i = 0; i < alphabet.length; i++) {
+			System.out.print(alphabet[i] + " ");
+		}
+	}
+}
