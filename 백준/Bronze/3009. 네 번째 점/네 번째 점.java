c77330fd701ce9005cd1d64d843ec40b15f64205
전화철 <117714970+J-Cheol@users.noreply.github.com>
2025-08-16 23:56:59 +0900

    [Bronze III] Title: 네 번째 점, Time: 64 ms, Memory: 11560 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int x1, y1, x2, y2, x3, y3;
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        x1 = Integer.parseInt(st.nextToken());
+        y1 = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        x2 = Integer.parseInt(st.nextToken());
+        y2 = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        x3 = Integer.parseInt(st.nextToken());
+        y3 = Integer.parseInt(st.nextToken());
+        
+        int x4;
+        if (x1 == x2) {
+            x4 = x3;
+        } else if (x1 == x3) {
+            x4 = x2;
+        } else {
+            x4 = x1;
+        }
+        
+        int y4;
+        if (y1 == y2) {
+            y4 = y3;
+        } else if (y1 == y3) {
+            y4 = y2;
+        } else {
+            y4 = y1;
+        }
+        
+        System.out.println(x4 + " " + y4);
+    }
+}
